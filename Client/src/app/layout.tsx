0f5c1fdb5fc4c2fbfb3be16ad02deaa963ed7812
Johnny8607 <dangjohnny0543@gmail.com>
2025-03-26 15:41:06 -0400

    complete simple payment feature, added a modetoggle for fun:)

--- Client/src/app/layout.tsx
+++ Client/src/app/layout.tsx
@@ -1,28 +1,37 @@
-import type React from "react"
-import "@/app/globals.css"
-import { Inter } from "next/font/google"
-import { ThemeProvider } from "@/components/theme-provider"
+import type React from "react";
+import "@/app/globals.css";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import ModeToggle from "./components/ModeToggle";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Smart Education Events System (SEES)",
-  description: "A cutting-edge platform for organizing, managing, and engaging with educational events",
-}
+  description:
+    "A cutting-edge platform for organizing, managing, and engaging with educational events",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+      <body className={`${inter.className}`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
+          <div className="fixed bottom-5 left-16">
+            <ModeToggle />
+          </div>
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
-
